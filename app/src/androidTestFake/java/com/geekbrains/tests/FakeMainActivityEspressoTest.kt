7cac2aa4e02310669aa09e8cbe24b2ef2a38804b
7nacu <7nacu7@gmail.com>
2022-07-19 00:05:31 +0300

    GBTestLesson4 ver. 0.4.4 -  избавился от проверки BuildConfig.TYPE == FAKE в тестах.

--- app/src/androidTestFake/java/com/geekbrains/tests/FakeMainActivityEspressoTest.kt
+++ app/src/androidTestFake/java/com/geekbrains/tests/FakeMainActivityEspressoTest.kt
@@ -1,23 +1,20 @@
 package com.geekbrains.tests
 
-import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.geekbrains.tests.view.search.MainActivity
-import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityEspressoTest {
+class FakeMainActivityEspressoTest {
 
     private lateinit var scenario: ActivityScenario<MainActivity>
 
@@ -31,19 +28,7 @@ class MainActivityEspressoTest {
         onView(withId(R.id.searchEditText)).perform(click())
         onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
-
-        onView(isRoot()).perform(delay())
-        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 3148")))
-    }
-
-    private fun delay(): ViewAction {
-        return object : ViewAction {
-            override fun getConstraints(): Matcher<View> = isRoot()
-            override fun getDescription(): String = "wait for $2 seconds"
-            override fun perform(uiController: UiController, v: View?) {
-                uiController.loopMainThreadForAtLeast(2000)
-            }
-        }
+        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
     }
 
     @After

91933b0c1cc46e8b221f7527bb41f1d1ce695342
Oleg Mysin <7nacu7@gmail.com>
2022-08-07 12:21:31 +0300

    GBTestLesson7 ver. 0.7.6 - протестирован RealRV;

--- app/src/androidTest/java/com/geekbrains/tests/MainActivityRecyclerViewTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/MainActivityRecyclerViewTest.kt
@@ -30,7 +30,7 @@ class MainActivityRecyclerViewTest {
     @Test
     fun activitySearch_ScrollTo() {
         if (BuildConfig.TYPE == MainActivity.FAKE) {
-            loadListTest()
+            fakeLoadListTest()
 
             onView(withId(R.id.recyclerView))
                 .perform(
@@ -44,7 +44,7 @@ class MainActivityRecyclerViewTest {
     @Test
     fun activitySearch_PerformClickAtPosition() {
         if (BuildConfig.TYPE == MainActivity.FAKE) {
-            loadListTest()
+            fakeLoadListTest()
 
             onView(withId(R.id.recyclerView))
                 .perform(
@@ -59,7 +59,7 @@ class MainActivityRecyclerViewTest {
     @Test
     fun activitySearch_PerformClickOnItem() {
         if (BuildConfig.TYPE == MainActivity.FAKE) {
-            loadListTest()
+            fakeLoadListTest()
 
             onView(withId(R.id.recyclerView))
                 .perform(
@@ -81,7 +81,7 @@ class MainActivityRecyclerViewTest {
     @Test
     fun activitySearch_PerformCustomClick() {
         if (BuildConfig.TYPE == MainActivity.FAKE) {
-            loadListTest()
+            fakeLoadListTest()
 
             onView(withId(R.id.recyclerView))
                 .perform(
@@ -94,9 +94,10 @@ class MainActivityRecyclerViewTest {
         }
     }
 
-    private fun loadListTest() {
+    private fun fakeLoadListTest() {
         onView(withId(R.id.searchEditText)).perform(click())
-        onView(withId(R.id.searchEditText)).perform(replaceText(TEST_FAKE_SEARCH), closeSoftKeyboard())
+        onView(withId(R.id.searchEditText)).perform(replaceText(TEST_FAKE_SEARCH),
+            closeSoftKeyboard())
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
     }
 
@@ -112,6 +113,46 @@ class MainActivityRecyclerViewTest {
         override fun perform(uiController: UiController, view: View) {
             val v = view.findViewById(id) as View
             v.performClick()
+        }
+    }
+
+    @Test
+    fun activitySearch_RealScrollToPosition() {
+        realLoadListTest()
+        onView(isRoot()).perform(delay())
+        onView(withId(R.id.recyclerView))
+            .perform(
+                RecyclerViewActions.scrollToPosition<SearchResultViewHolder>(77)
+            )
+    }
+
+    @Test
+    fun activitySearch_RealClickPosition() {
+        realLoadListTest()
+        onView(isRoot()).perform(delay())
+        onView(withId(R.id.recyclerView))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<SearchResultViewHolder>(
+                    0,
+                    click()
+                )
+            )
+    }
+
+    private fun realLoadListTest() {
+        onView(withId(R.id.searchEditText)).perform(click())
+        onView(withId(R.id.searchEditText)).perform(replaceText("RecyclerView"),
+            closeSoftKeyboard())
+        onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
+    }
+
+    private fun delay(): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> = isRoot()
+            override fun getDescription(): String = "wait for $3 seconds"
+            override fun perform(uiController: UiController, v: View?) {
+                uiController.loopMainThreadForAtLeast(3000)
+            }
         }
     }
 
